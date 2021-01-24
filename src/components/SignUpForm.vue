776e01caa9b2fb50f6b0211fa5e9892a8bfc431d
anjasmaradwisetiadi <enzoblackred@gmail.com>
2021-01-24 14:20:03 +0700

    initialize work

---
+++ src/components/SignUpForm.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="container-fluid h-100 mt-4">
+      <div class="row h-100">
+        <div class="col-md-3 h-100 d-flex justify-content-center align-items-center">
+          <h3>First Validation Input</h3>
+        </div>
+        <div class="col-md-9 h-100 d-flex justify-content-center align-items-center">
+          <div class="col-md-8 rounded px-5 py-4 shadow bg-white">
+            <p>Helo world </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+
+};
+
+</script>
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="home">
+    <p>naruto</p>
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  
+})
+</script>
